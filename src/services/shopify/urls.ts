b21ccf3ba07a0534b09a265f491b6f0ef9c82a46
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-12-18 19:09:31 -0600

    Proyecto: filtrando categorías de productos

--- src/services/shopify/urls.ts
+++ src/services/shopify/urls.ts
@@ -5,6 +5,7 @@ export const shopifyUrls = {
     'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-07/products.json`
   },
   collections: {
-    'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-07/smart_collections.json`
+    'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-07/smart_collections.json`,
+    'products': (id: string) => `${env.SHOPIFY_HOSTNAME}/admin/api/2023-07/collections/${id}/products.json`
   }
 }
