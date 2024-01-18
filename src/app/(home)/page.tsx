ecf010142738bd955a0b8a0ad8dfcb20ddd5010d
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2024-01-17 19:45:38 -0600

    Mejorando SEO de una página en Next.js

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,15 @@
 import { MainProducts } from "app/components/home/MainProducts"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "✨ Future world",
+  description: "Welcome to the future world, an ecommerce from other century",
+  keywords: ["ecommerce", "future", "world","technology"],
+}
 
 export default function Home() {
   return (
     <main>
-
       <MainProducts />
     </main>
   )
